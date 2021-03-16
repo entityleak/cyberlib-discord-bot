7d4c16ccb741cf2ebece6482e029ac6096016caf
entityleak <hello@son-la.co>
2021-03-16 18:01:23 +0100

    setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+  console.log('Bot is ready');
+});
+
+var urlSplitter = /(http[s]?:\/\/)?([^\/\s]+\/)(.*)/;
+
+client.on('message', (msg) => {
+  if (msg.content.includes('library.trust.support')){
+    
+    var match = urlSplitter.exec(msg.content);
+
+    var bookId = match[3];
+    console.log(match);
+    if(bookId){
+      msg.reply(bookId);
+    } else {
+      msg.reply('No book!');
+    }
+  } 
+});
