040720b439df15411a19ddb1712ddd5388d81fec
entityleak <hello@son-la.co>
2021-03-17 11:20:01 +0100

    fix

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const params = {
   spreadsheetId: process.env.SHEET_ID
 };
 
-https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get?apix_params=%7B%22spreadsheetId%22%3A%221vt1SlAfqV5cvWFVBFslCiNl5xyocLi9U31NuafCLw6g%22%2C%22includeGridData%22%3Afalse%7D
+// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get?apix_params=%7B%22spreadsheetId%22%3A%221vt1SlAfqV5cvWFVBFslCiNl5xyocLi9U31NuafCLw6g%22%2C%22includeGridData%22%3Afalse%7D
 
 async function main(params) {
   const res = await spreadsheet.spreadsheets.get({
